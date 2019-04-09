d04e909a82a14201299a2a1e305274b45eb77b7e
Robert-Paul Respicio <respicio.robertpaul@gmail.com>
2019-04-09 15:34:55 -0700

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import './App.scss';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
   render() {
     return (
       <React.Fragment>
@@ -11,16 +18,25 @@ class App extends Component {
             <div className="row">
               <div className="col">
                 <div className="drum-pad">
+                  <audio>
+                    <source src="" className="clip" id="Q" />
+                  </audio>
                   <p>Q</p>
                 </div>
               </div>
               <div className="col">
                 <div className="drum-pad">
+                  <audio>
+                    <source src="" className="clip" id="W" />
+                  </audio>
                   <p>W</p>
                 </div>
               </div>
               <div className="col">
                 <div className="drum-pad">
+                  <audio>
+                    <source src="" className="clip" id="E" />
+                  </audio>
                   <p>E</p>
                 </div>
               </div>
@@ -28,37 +44,63 @@ class App extends Component {
             <div className="row">
               <div className="col">
                 <div className="drum-pad">
+                  <audio>
+                    <source src="" className="clip" id="A" />
+                  </audio>
                   <p>A</p>
                 </div>
               </div>
               <div className="col">
                 <div className="drum-pad">
-                  <p>D</p>
+                  <audio>
+                    <source src="" className="clip" id="S" />
+                  </audio>
+                  <p>S</p>
                 </div>
               </div>
               <div className="col">
                 <div className="drum-pad">
-                  <p>F</p>
+                  <audio>
+                    <source src="" className="clip" id="DQ" />
+                  </audio>
+                  <p>D</p>
                 </div>
               </div>
             </div>
             <div className="row">
               <div className="col">
                 <div className="drum-pad">
+                  <audio>
+                    <source src="" className="clip" id="Z" />
+                  </audio>
                   <p>Z</p>
                 </div>
               </div>
               <div className="col">
                 <div className="drum-pad">
+                  <audio>
+                    <source src="" className="clip" id="X" />
+                  </audio>
                   <p>X</p>
                 </div>
               </div>
               <div className="col">
                 <div className="drum-pad">
+                  <audio>
+                    <source src="" className="clip" id="C" />
+                  </audio>
                   <p>C</p>
                 </div>
               </div>
             </div>
+          </div>
+          <div className="footer">
+            <p>
+              Created by {" "}
+              <a href="https://robertpaulrespicio.com/" target="_block">
+                Respici0
+              </a>
+            </p>
           </div>
         </div>
 
