4a4f04a2a43463bff7ca3ee313261d4be46a3ccb
Robert-Paul Respicio <respicio.robertpaul@gmail.com>
2019-04-10 11:41:39 -0700

    final drumkit

--- src/App.js
+++ src/App.js
@@ -5,85 +5,59 @@ const drumSounds = [
   {
     id: "Q",
     keyCode: 81,
-    soundName: "Heater-1",
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+    soundName: "heater-1",
   },
   {
     id: "W",
     keyCode: 87,
-    soundName: "Heater-2",
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+    soundName: "heater-2",
   },
   {
     id: "E",
     keyCode: 69,
-    soundName: "Heater-3",
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+    soundName: "heater-3",
   },
   {
     id: "A",
     keyCode: 65,
-    soundName: "Heater-4",
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+    soundName: "heater-4",
   },
   {
     id: "S",
     keyCode: 83,
-    soundName: "Heater-6",
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+    soundName: "heater-6",
   },
   {
     id: "D",
     keyCode: 68,
-    soundName: "Disc",
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+    soundName: "disc",
   },
   {
     id: "Z",
     keyCode: 90,
-    soundName: "Kick",
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+    soundName: "kick-1",
   },
   {
     id: "X",
     keyCode: 88,
-    soundName: "Kick-2",
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+    soundName: "kick-2",
   },
   {
     id: "C",
     keyCode: 67,
-    soundName: "Cev",
-    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+    soundName: "cev",
   }
 ]
-
-const audio = (props) => {
-  return (
-    <audio className="clip" id={props.id} >
-      <source src={props.url} type="audio/wav" />
-    </audio>
-  )
-}
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      drumSounds
+      id: "",
+      text: ""
     }
   }
 
-  // audio = (url, id) => {
-  //   return ({
-  //     __html: <div>
-  //       <audio>
-  //         <source src={url} type="audio/wav" className="clip" id={id} />
-  //       </audio>
-  //     </div>
-  //   }
-  //   )
-  // }
   componentDidMount = () => {
     document.addEventListener('keydown', this.handleKeyPress)
   }
@@ -94,162 +68,104 @@ class App extends Component {
 
   handleKeyPress = (e) => {
     let id;
-    switch (e.keyCode) {
-      case drumSounds[0].keyCode:
-        audio(drumSounds[0]);
-        id = drumSounds[0].id;
-        // this.playSound();
-        break;
-      case drumSounds[1].keyCode:
-        audio(drumSounds[1]);
-        id = drumSounds[1].id;
-        // this.playSound();
-        break;
-      case drumSounds[2].keyCode:
-        audio(drumSounds[2]);
-        id = drumSounds[2].id;
-        // this.playSound();
-        break;
-      case drumSounds[3].keyCode:
-        audio(drumSounds[3]);
-        id = drumSounds[3].id;
-        // this.playSound();
-        break;
-      case drumSounds[4].keyCode:
-        audio(drumSounds[4]);
-        id = drumSounds[4].id;
-        // this.playSound();
-        break;
-      case drumSounds[5].keyCode:
-        audio(drumSounds[5]);
-        id = drumSounds[5].id;
-        // this.playSound();
-        break;
-      case drumSounds[6].keyCode:
-        audio(drumSounds[6]);
-        id = drumSounds[6].id;
-        // this.playSound();
-        break;
-      case drumSounds[7].keyCode:
-        audio(drumSounds[7]);
-        id = drumSounds[7].id;
-        // this.playSound();
-        break;
-      case drumSounds[8].keyCode:
-        audio(drumSounds[8]);
-        id = drumSounds[8].id;
-        // this.playSound();
-        break;
-      case drumSounds[9].keyCode:
-        audio(drumSounds[9]);
-        id = drumSounds[9].id;
-        // this.playSound();
-        break;
-      default:
-        break;
+    let text;
+    let code = e.keyCode || e.which;
+
+    for (let i = 0; i < drumSounds.length; i++) {
+      if (code === drumSounds[i].keyCode || e.target.id === drumSounds[i].soundName) {
+        id = drumSounds[i].id;
+        text = drumSounds[i].soundName;
+        this.playSound(id);
+        this.setState({
+          id,
+          text,
+        });
+      }
     }
-    //console.log(id);
-    let x = document.getElementById(id)
-    //console.log(x);
+  }
+
+  playSound = (id) => {
+    document.getElementById(id).play();
+    setTimeout(this.revertButton, 200);
+  };
+
+  revertButton = () => {
     this.setState({
-      id: x
-    }, () => {
-      this.playSound();
+      id: ""
     })
-    //x.play();
   }
-
-
-  playSound = (e) => {
-    this.state.id.play();
-    console.log("pressed");
-
-  }
-  // return audio
-  // }
 
   render() {
+    // replace pressed with id === keyCode to change className accordingly 
+    const { id, text } = this.state
+    // refactor this code
     return (
       <React.Fragment>
         <div id="drum-machine">
-          <h1>BOOM CH BOOM-BOOM CH</h1>
+          <h1>BOOM BOX</h1>
           <div className="row">
             <div className="col">
-              <div className="drum-pad" id="heater-1" onClick={this.playSound} >
-                {/* <audio>
-                  <source src='https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3' type="audio/wav" className="clip" id="Q" />
-                </audio> */}
+              <div className={id !== "Q" ? "drum-pad" : "pressed"} id="heater-1" onClick={this.handleKeyPress} >
+                <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" type="audio/mp3" className="clip" id="Q" />
                 <p>Q</p>
               </div>
             </div>
             <div className="col">
-              <div className="drum-pad" onClick={this.playSound}>
-                {/* {audio} */}
+              <div className={id !== "W" ? "drum-pad" : "pressed"} id="heater-2" onClick={this.handleKeyPress} >
+                <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" type="audio/mp3" className="clip" id="W" />
                 <p>W</p>
               </div>
             </div>
             <div className="col">
-              <div className="drum-pad" onClick={this.playSound}>
-                <audio>
-                  <source src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" type="audio/wav" className="clip" id="E" />
-                </audio>
+              <div className={id !== "E" ? "drum-pad" : "pressed"} id="heater-3" onClick={this.handleKeyPress}>
+                <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" type="audio/mp3" className="clip" id="E" />
                 <p>E</p>
               </div>
             </div>
           </div>
           <div className="row">
             <div className="col">
-              <div className="drum-pad" onClick={this.playSound}>
-                <audio>
-                  <source src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" type="audio/wav" className="clip" id="A" />
-                </audio>
+              <div className={id !== "A" ? "drum-pad" : "pressed"} id="heater-4" onClick={this.handleKeyPress}>
                 <p>A</p>
+                <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" type="audio/mp3" className="clip" id="A" />
               </div>
             </div>
             <div className="col">
-              <div className="drum-pad" onClick={this.playSound}>
-                <audio>
-                  <source src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" type="audio/wav" className="clip" id="S" />
-                </audio>
+              <div className={id !== "S" ? "drum-pad" : "pressed"} id="heater-6" onClick={this.handleKeyPress}>
                 <p>S</p>
+                <audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3" type="audio/mp3" className="clip" id="S" />
               </div>
             </div>
             <div className="col">
-              <div className="drum-pad" onClick={this.playSound}>
-                <audio>
-                  <source src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" type="audio/wav" className="clip" id="DQ" />
-                </audio>
+              <div className={id !== "D" ? "drum-pad" : "pressed"} id="disc" onClick={this.handleKeyPress}>
                 <p>D</p>
+                <audio src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" type="audio/mp3" className="clip" id="D" />
               </div>
             </div>
           </div>
           <div className="row">
             <div className="col">
-              <div className="drum-pad" onClick={this.playSound}>
-                <audio>
-                  <source src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" type="audio/wav" className="clip" id="Z" />
-                </audio>
+              <div className={id !== "Z" ? "drum-pad" : "pressed"} id="kick-1" onClick={this.handleKeyPress}>
                 <p>Z</p>
+                <audio src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" type="audio/mp3" className="clip" id="Z" />
               </div>
             </div>
             <div className="col">
-              <div className="drum-pad" onClick={this.playSound}>
-                <audio>
-                  <source src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" type="audio/wav" className="clip" id="X" />
-                </audio>
+              <div className={id !== "X" ? "drum-pad" : "pressed"} id="kick-2" onClick={this.handleKeyPress}>
                 <p>X</p>
+                <audio src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" type="audio/mp3" className="clip" id="X" />
               </div>
             </div>
             <div className="col">
-              <div className="drum-pad" onClick={this.playSound}>
-                <audio>
-                  <source src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" type="audio/wav" className="clip" id="C" />
-                </audio>
+              <div className={id !== "C" ? "drum-pad" : "pressed"} id="cev" onClick={this.handleKeyPress}>
                 <p>C</p>
+                <audio src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" className="clip" id="C" />
               </div>
             </div>
           </div>
-          <div id="display" />
+          <div className="container" id="display">
+            {text}
+          </div>
         </div>
         <div className="footer">
           <p>
